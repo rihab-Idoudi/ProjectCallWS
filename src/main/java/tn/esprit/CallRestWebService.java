4c565385d4e23199bddba04052e3d3503a97deb2
Riha Id <rihab.idoudi@esprit.tn>
2022-02-18 12:37:18 +0100

    4th commit

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -22,6 +22,8 @@ import org.json.JSONObject;
 public class CallRestWebService {
 // 2nd commit
 	//3rd commit
+	//4th commit
+	
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
